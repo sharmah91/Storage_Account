74924075d14cad7d487c4c31b9fb9ce4139c773d
sharmah91 <sharmah91@gmail.com>
2025-05-26 16:30:44 +0530

    Storage Account File

---
+++ main.tf
@@ -0,0 +1,26 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~>4.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+  subscription_id = "5d5be31f-46ae-4693-866a-7cb188220117"
+}
+
+resource "azurerm_resource_group" "RG" {
+  name     = "RG_Hitesh_2"
+  location = "East US"
+}
+
+resource "azurerm_storage_account" "acc" {
+  name                     = "rgsaccount1"
+  resource_group_name      = azurerm_resource_group.RG.name
+  location                 = azurerm_resource_group.RG.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
